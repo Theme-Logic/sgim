78e606a82c07ddeaf95fe0deed51eb353a71918c
rjflavin <rjflavin@gmail.com>
2020-05-15 18:25:27 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -10,7 +10,9 @@ var SGIM = {
   },
   getCookie: function(name) {
     var nameEQ = name + "=";
+	console.log("NAMEEQ:"+nameEQ);
     var ca = document.cookie.split(";");
+	console.log("CA:"+ca);
     for (var i = 0; i < ca.length; i++) {
       var c = ca[i];
       while (c.charAt(0) == " ") c = c.substring(1, c.length);
